ef94b54e59088e5031ff27ede11b81351d48597c
OsvaldoRgonzalez <osvaldorgonzalez@lewisu.edu>
2016-10-12 21:22:12 -0500

    roomMoves

--- findingMinion.js
+++ findingMinion.js
@@ -10,10 +10,12 @@ function init() {
 	playerItems = [];
 	currentRoom = 0;
 	playerPossition = 0;
+	currentPosition = 0;
+
 	directionArray=['north','south','east','west'];
 	// Make an array that will contain room numbers accessible from a given room
 	//roomAccess = [N,E, S, W]
-	//-------------1------------2--------2-----------------------3-----------4---------------------5--------6--------
+	//-------------1------------2--------2------------------3-----------4---------------5--------6--------
 	roomAccess = [ ["north"], ["north","south","west"], ["east"],["south","east","west"],["south"],["south"] ]; // For just 2 rooms - you will need more
 	// This is how you will access the target room: roomAccess[currentRoom][dir]
 	// e.g., roomAcess[1][NORTH]
@@ -28,9 +30,9 @@ function init() {
 	"You're in the 1st bedroom on Second Floor. 'GO SOUTH' to get to Open Loft area",
 	"You're in the 2nd bedroom on Second Floor. 'GO SOUTH' to get to Open Loft area"];
 	minionPosition=["images/front.jpg","images/back.jpg","images/left.jpg","images/right.jpg"];
-
+	roomDescriptions[0];
 	// Below is an example of how to set the image based on current room
-	imageArray = ["images/entrance.jpg","images/livingroom.jpg", "images/kitchen.jpg", "images/openfloor", "images/bedroom", "images/bedroom2"]; 
+	imageArray = ["images/entrance.jpg","images/livingroom.jpg", "images/kitchen.jpg", "images/openfloor.jpg", "images/bedroom.jpg", "images/bedroom2.jpg"]; 
 	var theImage = document.getElementById("theImage");  // Get reference to <img> element
 	theImage.src = imageArray[currentRoom];  // Set the source
 
@@ -46,7 +48,79 @@ function scoreCount(){
 
 // You need a function like this to update room image, description, and items
 // Should be called after you change the current room
-function movementCheck(){
+function movementCheck(move){
+	// gets the location of the current room
+	console.log("current room: "+currentRoom);
+switch(currentRoom){
+	// room one movements
+	case 0:
+		//move in to living room
+		if(move == "north"){
+			currentRoom += 1;
+			console.log("Array position: "+currentRoom);
+		}
+			break;// this is the livingRoom
+	case 1:
+		// move upstairs
+		if (move == "north"){
+		
+			currentRoom += 2;
+			console.log("room 2: " +currentRoom);
+		}
+		
+		//move to back to living room
+		if(move == "south");
+		{
+			currentRoom -= 1;
+			console.log("room 2s: " +currentRoom);
+		}
+		if(move == "west");
+		{
+			currentRoom += 1;
+			console.log("room 2s: " +currentRoom);
+		}
+
+		break;
+	//kitchen 
+	case 2:
+		// go back to Living room
+		if(move == "south"){
+			currentRoom -=1;
+			console.log("room 3s: " +currentRoom);
+		}
+		break;
+	//openfloor
+	case 3:
+		// go to room 1
+		if (move == "east") {
+			currentRoom +=1;
+			console.log("room 4s: " +currentRoom);
+		}
+		// go to room 2
+		if(move == "west") {
+			currentRoom +=2;
+		}
+		// go to back to living Room
+		if(move == "south") {
+			currentRoom -= 2;
+		}
+		break;
+	//room 1
+	case 4:
+		// go back to openfloor
+		if(move == "south"){
+			currentRoom -=1;
+		}
+		break;
+	//room 2 
+	case 5:
+		// go back to open floor;
+		if(move == "south"){
+			currentRoom -=2;
+		}
+		break;
+		
+}
 
 }
 function roomChange(){
@@ -56,16 +130,13 @@ function roomChange(){
 }
 function refreshRoom(move) {
 
-for(var i = 0; i < roomAccess[currentRoom].length; i++){
-	console.log(roomAccess[currentRoom].length);
-	if (move == roomAccess[currentRoom][i]){
-		console.log(" hey you can move there");
-		console.log(roomAccess[currentRoom][i]);
-		currentRoom+=1;
-		theImage.src = imageArray[currentRoom];
-	}
-}
-	
+
+movementCheck(move);
+console.log("before chage: "+ currentRoom);
+theImage.src = imageArray[currentRoom];
+console.log("after change: "+ currentRoom);
+
+
 	
 	
 }
@@ -104,6 +175,7 @@ function parseCommand() {
 	if (ok[0] == true){
 		document.getElementById("error").innerHTML=" ";
 		refreshRoom(ok[1]);
+		
 		
 	}else{
 		document.getElementById("error").innerHTML="Please make sure you enter command in the right format: Go [North][South][East][West]";
