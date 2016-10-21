0d16d553b6e5b439c36b0e109afdc6afee0c6864
OsvaldoRgonzalez <osvaldorgonzalez@lewisu.edu>
2016-10-21 00:14:12 -0500

    new code
    added songs
    added new items
    end game screen
    and new start/drop stage

--- findingMinion.js
+++ findingMinion.js
@@ -1,94 +1,146 @@
 // Names:  	Osvaldo Gonzalez
 // 			Krystal Le
 // 	Project 2. Distributed and Web Programming. Fall 2016
+
 function init() {
 	// Initialize variables
 	currentRoom = 0; 
-	score_banana=0;//Keeps tracks of scores
+	totalScore=0;//Keeps tracks of scores
 	banana_count=0;//Keeps track of number of bananas 
-	randomBanana=0; //Put random number of bananas for each room
-	numArray=[]; //generate an array of 6 randomly generated ints
-	for (var i=0; i<6; i++){
+	apple_count=0;//keeps trakc of the number of apples
+	pear_count=0;//keeps trakc of the number of pears
+	potato_count=0;// keeps track of the number of potatos
+	// randomBanana=0; Put random number of bananas for each room
+	bananaLocArray=[0,1,0,1,1,1,0]; //generate an array of 6 randomly generated ints,
+	appleLocArray=[0,1,0,0,0,0,0];
+	pearLocArray=[0,1,0,1,0,0,1];
+	potatoLocArray=[0,0,1,0,1,0,1];
+	myItems = []; // array that will be filled with items collected by the minios
+	//==============================================random item placement========
+	/*for (var i=0; i<6; i++){
 		randomBanana=Math.floor((Math.random() *2)); 
 		numArray.push(randomBanana);
 
-	}
-	console.log(numArray);
+	*/
+	endAudio = new Audio("end.mp3");
+	alert("Hello player, the minions are very hungry and the evil genius Dr. Gru does not want to feed them. Therefore, Kevin has taken it upon himself to find all the food that Gru has hidden around the house. The minions have stolen a special device from Dr. Nefari in order to find all the food, but they are unable to read anything on it, and that is where you come in, help them read the device and feed the starving minions! They will pay you on points depending of the food that you collect(feel free to play music at the bottom of the screen)");
+	//console.log(bananaLocArray);
 	//Room descriptions
 	roomDescriptions =[ 
+	"use command 'drop items'  and score points for your fellow minios, or go north to enter house",
 	"You're in front of the apartment. 'GO NORTH' to enter the livingroom", 
 	"You're in the livingroom. 'GO WEST' to enter kitchen and 'GO NORTH' to go to the 2nd Floor",
 	"You're in the kitchen. 'GO SOUTH' to back out of kitchen to get to living room",
 	"You're in loft on Second Floor. 'GO WEST' to get to the 2st bedroom, 'GO EAST' to get to 1nd bedroom. 'GO SOUTH' to get to 1st Floor Livingroom", 
 	"You're in the 1st bedroom on Second Floor. 'GO SOUTH' to get to Open Loft area",
 	"You're in the 2nd bedroom on Second Floor. 'GO SOUTH' to get to Open Loft area",
-	"There is a banana in this room. 'TAKE BANANA' to get the banana and score some points"];
+	"There is a banana in this room. 'TAKE BANANA' to get the banana and score some points",
+	"congratulations you won! kevin found all the food and returned safe to his pals"];
 
 
 	//Prints the room descriptions starting at the first 
 	document.getElementById("description").innerHTML=roomDescriptions[0];
 	
 	// Below is an example of how to set the image based on current room
-	imageArray = ["images/entrance.jpg","images/livingroom.jpg", "images/kitchen.jpg", "images/openfloor.jpg", "images/bedroom.jpg", "images/bedroom2.jpg"]; 
+	imageArray = ["images/car.jpg", "images/entrance.jpg","images/livingroom.jpg", "images/kitchen.jpg", "images/openfloor.jpg", "images/bedroom.jpg", "images/bedroom2.jpg", "images/endGame.jpg"]; 
 	var theImage = document.getElementById("theImage");  // Get reference to <img> element
 	theImage.src = imageArray[currentRoom];  // Set the source
 
 	//Gets the minion to be placed in to the game
 	minion=document.getElementById("minion");
 	minion.src="images/minion5.jpg";
-	checkForBanana(currentRoom, 0);
+	checkForItems(currentRoom, 0);
 
 	
 }
 //This function checks whether there is a banana in the room and perform neccessary 
 //action such as prompting user to "take the banana an score points"
-function checkForBanana(currentRoom, myitem){
-	
-	console.log("banananum" + numArray[currentRoom]);
-	if (numArray[currentRoom]!=0) {
-		document.getElementById("bananaFound").innerHTML=roomDescriptions[6];
-		console.log("myitem"+myitem);
-		if (myitem=="take") {
-			console.log(numArray);
-			score(numArray[currentRoom]);
-		}
-	}if (numArray[currentRoom]==0) {
-		console.log(numArray[currentRoom]);
-		document.getElementById("bananaFound").innerHTML='There is no banana in the room';
+function checkForItems(currentRoom, actions, myitem){
+	if (bananaLocArray[currentRoom] != 0 || appleLocArray[currentRoom] !=0 || pearLocArray[currentRoom] !=0 || potatoLocArray[currentRoom] != 0 )  {
+		document.getElementById("bananaFound").innerHTML=roomDescriptions[7];
+		if (actions=="take") {
+			switch(myitem){
+				case "apple":{
+					if(appleLocArray[currentRoom] != 0){
+						appleLocArray[currentRoom] = 0;
+						apple_count += 1;
+						document.getElementById("appleCount").innerHTML=apple_count;
+					}
+					break;
+				}
+				case "banana":{
+					if(bananaLocArray[currentRoom] != 0){
+						bananaLocArray[currentRoom]=0;
+						banana_count +=1;
+						document.getElementById("bananaCount").innerHTML=banana_count;
+					}
+					break;
+				}
+				case "pear":{
+					if (pearLocArray[currentRoom] != 0){
+						pearLocArray[currentRoom] = 0;
+						pear_count +=1;
+						document.getElementById("pearCount").innerHTML=pear_count;
+					}
+					break;
+				}
+				case "potato":{
+					if (potatoLocArray[currentRoom] != 0){
+						potatoLocArray[currentRoom] = 0;
+						potato_count +=1;
+						document.getElementById("potatoCount").innerHTML=potato_count;
+					}
+					break;
+					}
+				}
+			}
+	}if (bananaLocArray[currentRoom] == 0 && appleLocArray[currentRoom] ==0 && pearLocArray[currentRoom] ==0 && potatoLocArray[currentRoom] == 0) {
+		document.getElementById("bananaFound").innerHTML='There are no items here ';
+	}else{
+		document.getElementById("bananaFound").innerHTML="There are: bananas["+bananaLocArray[currentRoom]+"] apples["+appleLocArray[currentRoom]+"] pear["+pearLocArray[currentRoom] +"] potato["+potatoLocArray[currentRoom]+"], (Use take + [food name] to get item)";
 	}
 }
-//This function perform the scoring, for each banana collected, user scores 10 points.
-function score(numberOfBanana){
-
-	score_banana=score_banana+(numberOfBanana*10)
-
-	document.getElementById("score").innerHTML=score_banana;
-	banana_count=banana_count+numberOfBanana;
-
-	document.getElementById("bananaCount").innerHTML=banana_count;
-	console.log("banana"+numArray[currentRoom]);
-
-	numArray[currentRoom]=0;
-	console.log(numArray);
-	document.getElementById("bananaFound").innerHTML='There is no banana left in the room';
-
-
-
+//This function perform the scoring and updating, it also check if all items have been collected 
+function score(){
+	// Apples=5points Bannas=10points Pears = 3points Potatos = 2poiints
+	totalScore=totalScore+(potato_count*2)+(banana_count*10)+(pear_count*3)+(apple_count*5)
+	document.getElementById("score").innerHTML=totalScore;
+	//=== set the number of collected items to 0
+	document.getElementById("appleCount").innerHTML=apple_count=0;	
+	document.getElementById("bananaCount").innerHTML=banana_count=0;	
+	document.getElementById("pearCount").innerHTML=pear_count=0;	
+	document.getElementById("potatoCount").innerHTML=potato_count=0;	
+	document.getElementById("bananaFound").innerHTML='Items droped to the car';
+	// checks if all arrays are empty
+	if (bananaLocArray.includes(1)== false && appleLocArray.includes(1)== false && pearLocArray.includes(1)==false && potatoLocArray.includes(1)==false){
+		theImage.src = imageArray[7];
+		document.getElementById("description").innerHTML=roomDescriptions[8];
+		document.getElementById("bananaFound").innerHTML='reload screen to play again';
+		endAudio.play();
+	}
 }
 //This function move rooms based on user's current position and command entered.
 function movementCheck(move){
 	// gets the location of the current room
-	
 	switch(currentRoom){
 	// room one movements
 	case 0:
+		//move to the entrance
+		if(move == "north"){
+			currentRoom += 1;
+		}
+			break;// this is the livingRoom
+	case 1:
 		//move in to living room
 		if(move == "north"){
 			currentRoom += 1;
 			
 		}
+		if(move =="south"){
+			currentRoom -= 1;
+		}
 			break;// this is the livingRoom
-			case 1:
+	case 2:
 		// move upstairs
 		if (move == "north"){
 
@@ -108,7 +160,7 @@ function movementCheck(move){
 
 		break;
 	//kitchen 
-	case 2:
+	case 3:
 		// go back to Living room
 		if(move == "south"){
 			currentRoom -=1;
@@ -116,7 +168,7 @@ function movementCheck(move){
 		}
 		break;
 	//openfloor
-	case 3:
+	case 4:
 		// go to room 1
 		if (move == "east") {
 			currentRoom +=1;
@@ -132,14 +184,14 @@ function movementCheck(move){
 		}
 		break;
 	//room 1
-	case 4:
+	case 5:
 		// go back to openfloor
 		if(move == "south"){
 			currentRoom -=1;
 		}
 		break;
 	//room 2 
-	case 5:
+	case 6:
 		// go back to open floor;
 		if(move == "south"){
 			currentRoom -=2;
@@ -152,50 +204,54 @@ function movementCheck(move){
 function refreshRoom(move) {
 	movementCheck(move);
 	theImage.src = imageArray[currentRoom];
-	theImage.src = minionArray[currentRoom];
 	document.getElementById("description").innerHTML=roomDescriptions[currentRoom];
 
 }
 
 
 //This function parses, validates commands entered by users.
-function parseCommand(){
+function parseCommand() {
 	var command=document.getElementById("command").value;
 	//Array of directions, used to determine what to do with user input
 	var directionArray = ["north","west","east","south"];
 	//Checks if the first word is go
-	//var isAction  = command.search(/^go /);
-	
+	var myfruits = ["apple","banana","pear","potato"];
 	var isDirection = "";
 	if (command.search(/^go /) == 0){
 		command=command.split(' ');
 		var direction=command[1].toLowerCase();
 		// Checks if the command not a number or blank 
 		isDirection = direction.search(/\w/)
-		//Chen it checks if the second argument is a valid movement EX: west, north south east
+		//checks if the second argument is a valid movement EX: west, north south east
 		if(isDirection == 0 ){
 			for (var i=0; i<directionArray.length; i++){
 				if (direction == directionArray[i]){
 					refreshRoom(direction);
-					checkForBanana(currentRoom, 0);
+					checkForItems(currentRoom, 0);
 				}
 			}
 		}
 	}
-	//console.log("action1"+isAction);
-	//Cheks if the user wants to get a banana
-	//isAction  = command.search(/^take /);
-	//console.log("action2"+isAction);
-	else if( command.search(/^take /)== 0){
+	//Cheks if the user wants to get a items
+	else if( command.search(/^take /)== 0)	{
 		command=command.split(' ');
 		var myitem=command[1].toLowerCase();
-		if( myitem =="banana"){
-			// pass current room and banana 
-			checkForBanana(currentRoom, command[0]);
+		// compares of the user entered the right item after take, 
+		for (var i = 0; i<4; i++ )	{
+			if( myitem == myfruits[i]) {
+				// pass current room and banana 
+				checkForItems(currentRoom, command[0], myitem );
+			}
 		}
-	}else{
+	}else if(command.search(/^drop /)== 0) {
+		command=command.split(' ');
+		var drops=command[1].toLowerCase();
+		if(drops == "items" && currentRoom==0) {
+			score();
+		}
+	} else {
 	//if nothing works then break 
-	document.getElementById("description").innerHTML="Please make sure you enter command in the right format: Go [North][South][East][West]";
+	document.getElementById("description").innerHTML="Eror enter the correct format: Go[North][South][East][West] or take[banana][apple][pear][potato]";
 }
 }
 
